c6406baa2455cc2ef9ac5f7fb77910e2d1fadf69
Adam <adamfgcross@gmail.com>
2024-08-06 13:15:49 -0400

    create notes model and keyword

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    keywords = models.ManyToManyField('Keyword', related_name='notes')
+    
+class Keyword(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    text = models.CharField(max_length=255)
+    

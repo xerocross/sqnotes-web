f64b34ae720fe60c2e653001c7edc6b9ad36a64d
Adam <adamfgcross@gmail.com>
2024-08-06 13:17:49 -0400

    correct string for notes.Keyword

--- notes/models.py
+++ notes/models.py
@@ -5,7 +5,7 @@ class Note(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
-    keywords = models.ManyToManyField('Keyword', related_name='notes')
+    keywords = models.ManyToManyField('notes.Keyword', related_name='notes')
     
 class Keyword(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
